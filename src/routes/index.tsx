a38b931efb0bb365b9903bb9c29afbdf375853f2
MasterTuto <brenocarvalho2011@gmail.com>
2023-02-21 13:49:30 -0300

    fix: removed unused router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, HashRouter } from "react-router-dom"
+import { Route, Routes, HashRouter } from "react-router-dom"
 import MainLayout from "../layouts/MainLayout"
 import AboutMe from "../screens/AboutMe"
 import Contact from "../screens/Contact"
